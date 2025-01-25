d643b1bf55c977957b91f97db113943da9f8d06c
JesusArm98 <armando.estrada@tuvanosa.com>
2025-01-24 18:07:16 -0700

    Cambio de layout principal

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,7 +30,7 @@ export const metadata = {
     images: [
       {
         url: 'https://firebasestorage.googleapis.com/v0/b/fir-adminsdk-documents.appspot.com/o/Myllos.webp?alt=media&token=286f024b-6144-4152-a657-29966c67be2f',
-        width: 1200,
+        width: 1000,
         height: 630,
         alt: 'Myllos - Soluciones Logisticas',
       },
