b5a6ca77b919ffefb94378d5099dabacb1f05ab9
JesArm98 <armando.aramburo75@gmail.com>
2025-01-29 22:43:03 -0700

    Prueba SEO 100

--- src/app/page.js
+++ src/app/page.js
@@ -8,29 +8,6 @@ import FAQ from "@/ui/sections/FAQ/FAQ";
 import Estadisticas from "@/ui/sections/Estadisticas/Estadisticas";
 import ScrollToContact from "@/ui/components/ScrollToContact/ScrollToContact";
 
-export const metadata = {
-  title: "Myllos - Soluciones Logísticas",
-  description:
-    "Empresa dedicada y especializada en aportar la mejor solución para sus problemáticas de envíos de mercancía por distintos tipos de transporte.",
-  openGraph: {
-    title: "Myllos - Soluciones Logísticas",
-    description:
-      "Empresa dedicada y especializada en aportar la mejor solución para sus problemáticas de envíos de mercancía por distintos tipos de transporte.",
-    url: "https://myllos.netlify.app/",
-    type: "website",
-    images: [
-      {
-        url: "https://firebasestorage.googleapis.com/v0/b/fir-adminsdk-documents.appspot.com/o/Myllos.jpg?alt=media&token=80a669b8-699c-49ce-8747-8b9367fcef4b",
-        width: 1000,
-        height: 630,
-        alt: "Myllos - Soluciones Logísticas",
-        type: "image/jpeg",
-      },
-    ],
-  },
-};
-
-
 export default function Home() {
   return (
     <main>
